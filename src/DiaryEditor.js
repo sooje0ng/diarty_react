329e3e72b59ff18866357bc41921bddaae00cc66
soojeong <tnwjd7890@gmail.com>
2022-06-20 20:14:46 +0900

    DOM조작

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: '',
     content: '',
@@ -13,7 +16,15 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus(); //입력창에 포커스를 줌
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus(); //입력창에 포커스를 줌
+      return;
+    }
     alert('저장 완료');
   };
   return (
@@ -21,6 +32,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -28,6 +40,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
