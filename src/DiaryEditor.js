7ec46d5d88f2f1298f7c59346c8e1e69049c3289
soojeong <tnwjd7890@gmail.com>
2022-06-20 18:02:41 +0900

    select 추가, 간단한 css

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -3,12 +3,18 @@ const DiaryEditor = () => {
   const [state, setState] = useState({
     author: '',
     content: '',
+    emotion: 1,
   });
   const handleChangeState = (e) => {
     setState({
       ...state,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert('저장 완료');
   };
   return (
     <div className="DiaryEditor">
@@ -19,13 +25,30 @@ const DiaryEditor = () => {
           value={state.author}
           onChange={handleChangeState}
         />
-        <div>
-          <textarea
-            name="content"
-            value={state.content}
-            onChange={handleChangeState}
-          />
-        </div>
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        오늘의 점수 :{' '}
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>저장</button>
       </div>
     </div>
   );
